5fa1b08f6d04dc77d2e54e61a32fddd712ae9588
Kishan-Dhakan <kishandhakan17@gmail.com>
2021-05-28 02:00:15 +0530

    initial commit ls cmd

--- cmd/list.go
+++ cmd/list.go
@@ -11,133 +11,136 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// Run function in listCmd
+func list(cmd *cobra.Command, args []string) {
+	fflags := cmd.Flags() // fflags is a *flag.FlagSet
+	if fflags.Changed("remotepath") == false && fflags.Changed("authticket") == false {
+		PrintError("Error: remotepath / authticket flag is missing")
+		os.Exit(1)
+	}
+
+	remotepath := cmd.Flag("remotepath").Value.String()
+	authticket := cmd.Flag("authticket").Value.String()
+	lookuphash := cmd.Flag("lookuphash").Value.String()
+	doJSON, _ := cmd.Flags().GetBool("json")
+	if len(remotepath) == 0 && (len(authticket) == 0) {
+		PrintError("Error: remotepath / authticket / lookuphash flag is missing")
+		os.Exit(1)
+	}
+
+	if len(remotepath) > 0 {
+		if fflags.Changed("allocation") == false { // check if the flag "path" is set
+			PrintError("Error: allocation flag is missing") // If not, we'll let the user know
+			os.Exit(1)                                      // and os.Exit(1)
+		}
+		allocationID := cmd.Flag("allocation").Value.String()
+		allocationObj, err := sdk.GetAllocation(allocationID)
+		if err != nil {
+			PrintError("Error fetching the allocation", err)
+			os.Exit(1)
+		}
+		remotepath := cmd.Flag("remotepath").Value.String()
+		ref, err := allocationObj.ListDir(remotepath)
+		if err != nil {
+			PrintError(err.Error())
+			os.Exit(1)
+		}
+		if doJSON {
+			util.PrintJSON(ref.Children)
+			return
+		}
+		header := []string{"Type", "Name", "Path", "Size", "Num Blocks", "Lookup Hash", "Is Encrypted", "Downloads payer"}
+		data := make([][]string, len(ref.Children))
+		for idx, child := range ref.Children {
+			size := strconv.FormatInt(child.Size, 10)
+			if child.Type == fileref.DIRECTORY {
+				size = ""
+			}
+			isEncrypted := ""
+			if child.Type == fileref.FILE {
+				if len(child.EncryptionKey) > 0 {
+					isEncrypted = "YES"
+				} else {
+					isEncrypted = "NO"
+				}
+			}
+			data[idx] = []string{
+				child.Type,
+				child.Name,
+				child.Path,
+				size,
+				strconv.FormatInt(child.NumBlocks, 10),
+				child.LookupHash,
+				isEncrypted,
+				child.Attributes.WhoPaysForReads.String(),
+			}
+		}
+		util.WriteTable(os.Stdout, header, []string{}, data)
+	} else if len(authticket) > 0 {
+		allocationObj, err := sdk.GetAllocationFromAuthTicket(authticket)
+		if err != nil {
+			PrintError("Error fetching the allocation", err)
+			os.Exit(1)
+		}
+		at := sdk.InitAuthTicket(authticket)
+		isDir, err := at.IsDir()
+		if isDir && len(lookuphash) == 0 {
+			lookuphash, err = at.GetLookupHash()
+			if err != nil {
+				PrintError("Error getting the lookuphash from authticket", err)
+				os.Exit(1)
+			}
+		}
+		if !isDir {
+			PrintError("Invalid operation. Auth ticket is not for a directory")
+			os.Exit(1)
+		}
+
+		ref, err := allocationObj.ListDirFromAuthTicket(authticket, lookuphash)
+		if err != nil {
+			PrintError(err.Error())
+			os.Exit(1)
+		}
+
+		if doJSON {
+			util.PrintJSON(ref.Children)
+			return
+		}
+		header := []string{"Type", "Name", "Size", "Num Blocks", "Lookup Hash", "Is Encrypted", "Downloads payer"}
+		data := make([][]string, len(ref.Children))
+		for idx, child := range ref.Children {
+			size := strconv.FormatInt(child.Size, 10)
+			isEncrypted := ""
+			if child.Type == fileref.FILE {
+				if len(child.EncryptionKey) > 0 {
+					isEncrypted = "YES"
+				} else {
+					isEncrypted = "NO"
+				}
+			}
+			data[idx] = []string{
+				child.Type,
+				child.Name,
+				size,
+				strconv.FormatInt(child.NumBlocks, 10),
+				child.LookupHash,
+				isEncrypted,
+				child.Attributes.WhoPaysForReads.String(),
+			}
+		}
+		util.WriteTable(os.Stdout, header, []string{}, data)
+	}
+
+	return
+}
+
 // listCmd represents list command
 var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "list files from blobbers",
 	Long:  `list files from blobbers`,
 	Args:  cobra.MinimumNArgs(0),
-	Run: func(cmd *cobra.Command, args []string) {
-		fflags := cmd.Flags() // fflags is a *flag.FlagSet
-		if fflags.Changed("remotepath") == false && fflags.Changed("authticket") == false {
-			PrintError("Error: remotepath / authticket flag is missing")
-			os.Exit(1)
-		}
-
-		remotepath := cmd.Flag("remotepath").Value.String()
-		authticket := cmd.Flag("authticket").Value.String()
-		lookuphash := cmd.Flag("lookuphash").Value.String()
-		doJSON, _ := cmd.Flags().GetBool("json")
-		if len(remotepath) == 0 && (len(authticket) == 0) {
-			PrintError("Error: remotepath / authticket / lookuphash flag is missing")
-			os.Exit(1)
-		}
-
-		if len(remotepath) > 0 {
-			if fflags.Changed("allocation") == false { // check if the flag "path" is set
-				PrintError("Error: allocation flag is missing") // If not, we'll let the user know
-				os.Exit(1)                                      // and os.Exit(1)
-			}
-			allocationID := cmd.Flag("allocation").Value.String()
-			allocationObj, err := sdk.GetAllocation(allocationID)
-			if err != nil {
-				PrintError("Error fetching the allocation", err)
-				os.Exit(1)
-			}
-			remotepath := cmd.Flag("remotepath").Value.String()
-			ref, err := allocationObj.ListDir(remotepath)
-			if err != nil {
-				PrintError(err.Error())
-				os.Exit(1)
-			}
-			if doJSON {
-				util.PrintJSON(ref.Children)
-				return
-			}
-			header := []string{"Type", "Name", "Path", "Size", "Num Blocks", "Lookup Hash", "Is Encrypted", "Downloads payer"}
-			data := make([][]string, len(ref.Children))
-			for idx, child := range ref.Children {
-				size := strconv.FormatInt(child.Size, 10)
-				if child.Type == fileref.DIRECTORY {
-					size = ""
-				}
-				isEncrypted := ""
-				if child.Type == fileref.FILE {
-					if len(child.EncryptionKey) > 0 {
-						isEncrypted = "YES"
-					} else {
-						isEncrypted = "NO"
-					}
-				}
-				data[idx] = []string{
-					child.Type,
-					child.Name,
-					child.Path,
-					size,
-					strconv.FormatInt(child.NumBlocks, 10),
-					child.LookupHash,
-					isEncrypted,
-					child.Attributes.WhoPaysForReads.String(),
-				}
-			}
-			util.WriteTable(os.Stdout, header, []string{}, data)
-		} else if len(authticket) > 0 {
-			allocationObj, err := sdk.GetAllocationFromAuthTicket(authticket)
-			if err != nil {
-				PrintError("Error fetching the allocation", err)
-				os.Exit(1)
-			}
-			at := sdk.InitAuthTicket(authticket)
-			isDir, err := at.IsDir()
-			if isDir && len(lookuphash) == 0 {
-				lookuphash, err = at.GetLookupHash()
-				if err != nil {
-					PrintError("Error getting the lookuphash from authticket", err)
-					os.Exit(1)
-				}
-			}
-			if !isDir {
-				PrintError("Invalid operation. Auth ticket is not for a directory")
-				os.Exit(1)
-			}
-
-			ref, err := allocationObj.ListDirFromAuthTicket(authticket, lookuphash)
-			if err != nil {
-				PrintError(err.Error())
-				os.Exit(1)
-			}
-
-			if doJSON {
-				util.PrintJSON(ref.Children)
-				return
-			}
-			header := []string{"Type", "Name", "Size", "Num Blocks", "Lookup Hash", "Is Encrypted", "Downloads payer"}
-			data := make([][]string, len(ref.Children))
-			for idx, child := range ref.Children {
-				size := strconv.FormatInt(child.Size, 10)
-				isEncrypted := ""
-				if child.Type == fileref.FILE {
-					if len(child.EncryptionKey) > 0 {
-						isEncrypted = "YES"
-					} else {
-						isEncrypted = "NO"
-					}
-				}
-				data[idx] = []string{
-					child.Type,
-					child.Name,
-					size,
-					strconv.FormatInt(child.NumBlocks, 10),
-					child.LookupHash,
-					isEncrypted,
-					child.Attributes.WhoPaysForReads.String(),
-				}
-			}
-			util.WriteTable(os.Stdout, header, []string{}, data)
-		}
-
-		return
-	},
+	Run:   list,
 }
 
 var listAllCmd = &cobra.Command{
--- cmd/listallocations.go
+++ cmd/listallocations.go
@@ -12,54 +12,56 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func listallocations(cmd *cobra.Command, args []string) {
+	doJSON, _ := cmd.Flags().GetBool("json")
+
+	allocations, err := sdk.GetAllocations()
+	if err != nil {
+		PrintError("Error getting allocations list." + err.Error())
+		os.Exit(1)
+	}
+	if doJSON {
+		util.PrintJSON(allocations)
+		return
+	}
+	header := []string{"ID", "Size", "Expiration", "Datashards",
+		"Parityshards", "Finalized", "Canceled", "R. Price", "W. Price"}
+	data := make([][]string, len(allocations))
+	for idx, allocation := range allocations {
+		size := strconv.FormatInt(allocation.Size, 10)
+		expStr := strconv.FormatInt(allocation.Expiration, 10)
+		exp, err := strconv.ParseInt(expStr, 10, 64)
+		if err == nil {
+			tm := time.Unix(exp, 0)
+			expStr = tm.String()
+		}
+		d := strconv.FormatInt(int64(allocation.DataShards), 10)
+		p := strconv.FormatInt(int64(allocation.ParityShards), 10)
+
+		// TODO (sfxdx): data shards, parity shards
+
+		var rp, wp common.Balance
+		for _, d := range allocation.BlobberDetails {
+			rp += d.Terms.ReadPrice
+			wp += d.Terms.WritePrice
+		}
+
+		data[idx] = []string{
+			allocation.ID, size, expStr, d, p,
+			strconv.FormatBool(allocation.Finalized),
+			strconv.FormatBool(allocation.Canceled),
+			rp.String(), wp.String(),
+		}
+	}
+	util.WriteTable(os.Stdout, header, []string{}, data)
+	return
+}
+
 var listallocationsCmd = &cobra.Command{
 	Use:   "listallocations",
 	Short: "List allocations for the client",
 	Long:  `List allocations for the client`,
-	Run: func(cmd *cobra.Command, args []string) {
-		doJSON, _ := cmd.Flags().GetBool("json")
-
-		allocations, err := sdk.GetAllocations()
-		if err != nil {
-			PrintError("Error getting allocations list." + err.Error())
-			os.Exit(1)
-		}
-		if doJSON {
-			util.PrintJSON(allocations)
-			return
-		}
-		header := []string{"ID", "Size", "Expiration", "Datashards",
-			"Parityshards", "Finalized", "Canceled", "R. Price", "W. Price"}
-		data := make([][]string, len(allocations))
-		for idx, allocation := range allocations {
-			size := strconv.FormatInt(allocation.Size, 10)
-			expStr := strconv.FormatInt(allocation.Expiration, 10)
-			exp, err := strconv.ParseInt(expStr, 10, 64)
-			if err == nil {
-				tm := time.Unix(exp, 0)
-				expStr = tm.String()
-			}
-			d := strconv.FormatInt(int64(allocation.DataShards), 10)
-			p := strconv.FormatInt(int64(allocation.ParityShards), 10)
-
-			// TODO (sfxdx): data shards, parity shards
-
-			var rp, wp common.Balance
-			for _, d := range allocation.BlobberDetails {
-				rp += d.Terms.ReadPrice
-				wp += d.Terms.WritePrice
-			}
-
-			data[idx] = []string{
-				allocation.ID, size, expStr, d, p,
-				strconv.FormatBool(allocation.Finalized),
-				strconv.FormatBool(allocation.Canceled),
-				rp.String(), wp.String(),
-			}
-		}
-		util.WriteTable(os.Stdout, header, []string{}, data)
-		return
-	},
+	Run:   listallocations,
 }
 
 func init() {
